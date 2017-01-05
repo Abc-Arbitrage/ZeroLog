cafa0e2fb35722e2696eaec125e52e50f48da9c1
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2017-01-05 11:15:00 +0100

    Add performance test with multiple threads

---
+++ ZeroLog.Tests/PerformanceTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ZeroLog.Appenders;
+
+namespace ZeroLog.Tests
+{
+    [TestFixture]
+    [Ignore("Manual")]
+    public class PerformanceTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+//            LogManager.Initialize(new[] { new DateAndSizeRollingFileAppender("PerfTest"), }, 100 * 1024);
+            LogManager.Initialize(new[] { new NullAppender(), }, 100 * 1024);
+            Thread.Sleep(1);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            LogManager.Shutdown();
+        }
+
+        [Test]
+        public void should_test_console()
+        {
+            LogManager.GetLogger(typeof(PerformanceTests)).Info().Append("Hello ").Append(42).Append(" this is a relatlively long message ").Append(12345.4332m).Log();
+        }
+
+        [Test]
+        public void should_run_test()
+        {
+            const int count = 100000;
+            const int threads = 4;
+
+            var timer = Stopwatch.StartNew();
+
+            var logger = LogManager.GetLogger(typeof(PerformanceTests));
+
+            Parallel.For(0, threads, i =>
+            {
+                for (int k = 0; k < count; k++)
+                {
+                    Thread.Sleep(1);
+                    var logEvent = logger.Info().Append("Hello ").Append(42).Append(" this is a relatlively long message ").Append(12345.4332m);
+                    logEvent.Log();
+                }
+            });
+
+            LogManager.Shutdown();
+            timer.Stop();
+            Console.WriteLine("Log  : {0} us/log", timer.ElapsedMilliseconds * 1000.0 / (count * threads));
+        }
+
+    }
+}
---
+++ ZeroLog/Appenders/NullAppender.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ZeroLog.Appenders
+{
+    public class NullAppender : AppenderBase
+    {
+        private readonly MemoryStream _output = new MemoryStream(1024);
+
+        public override void WriteEvent(LogEvent logEvent, byte[] messageBytes, int messageLength)
+        {
+            _output.Seek(0, SeekOrigin.Begin);
+
+            WritePrefix(_output, logEvent);
+
+            NewlineBytes.CopyTo(messageBytes, messageLength);
+            messageLength += NewlineBytes.Length;
+
+            _output.Write(messageBytes, 0, messageLength);
+        }
+    }
+}
