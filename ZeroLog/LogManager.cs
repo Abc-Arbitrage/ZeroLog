cb171d6b60709a4acb8979e37522622bf63afcf8
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2017-01-06 17:42:18 +0100

    Add performance appender to measure total log time percentiles

--- ZeroLog/LogManager.cs
+++ ZeroLog/LogManager.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.Formatting;
 using System.Threading.Tasks;
@@ -86,7 +84,7 @@ namespace ZeroLog
         {
             var stringBuffer = new StringBuffer();
             var destination = new byte[1024];
-            while (_isRunning)
+            while (_isRunning || !_queue.IsEmpty)
             {
                 try
                 {
@@ -123,61 +121,5 @@ namespace ZeroLog
                 _pool.Free(logEvent);
             }
         }
-    }
-
-    internal class FakeCollection : ICollection<LogEvent>
-    {
-        private readonly int _size;
-
-        public FakeCollection(int size)
-        {
-            _size = size;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public IEnumerator<LogEvent> GetEnumerator()
-        {
-            return Enumerable.Empty<LogEvent>().GetEnumerator();
-        }
-
-        public void CopyTo(Array array, int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Count => _size;
-        public object SyncRoot { get; }
-        public bool IsSynchronized { get; }
-
-        public void Add(LogEvent item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(LogEvent item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(LogEvent[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(LogEvent item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsReadOnly { get; }
     }
 }
