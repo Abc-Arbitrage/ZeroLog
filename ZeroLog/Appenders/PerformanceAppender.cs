0b84cbaaf5c80e44ba02eb6a60c002ce43956e80
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2017-01-06 17:42:18 +0100

    Add performance appender to measure total log time percentiles

---
+++ ZeroLog/Appenders/PerformanceAppender.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ZeroLog.Appenders
+{
+    public class PerformanceAppender : IAppender
+    {
+        private readonly MessageReceived[] _messages;
+        private int _count;
+
+        public PerformanceAppender(int expectedEntries)
+        {
+            _messages = new MessageReceived[expectedEntries];
+            for (int i = 0; i < expectedEntries; i++)
+            {
+                _messages[i] = new MessageReceived(new byte[30]);
+            }
+        }
+
+        public void WriteEvent(LogEvent logEvent, byte[] messageBytes, int messageLength)
+        {
+            Array.Copy(messageBytes, _messages[_count].StartTimestampInChars, messageLength);
+            _messages[_count].MessageLength = messageLength;
+            _messages[_count].EndTimestamp = Stopwatch.GetTimestamp();
+            _count++;
+        }
+
+        public void SetEncoding(Encoding encoding)
+        {
+        }
+
+        private struct MessageReceived
+        {
+            public readonly byte[] StartTimestampInChars;
+            public int MessageLength;
+            public long EndTimestamp;
+
+            public MessageReceived(byte[] startTimestampInChars)
+            {
+                StartTimestampInChars = startTimestampInChars;
+                EndTimestamp = 0;
+                MessageLength = 0;
+            }
+        }
+
+        public void PrintTimeTaken()
+        {
+            using (var fileStream = new StreamWriter(File.OpenWrite("total-time.csv")))
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    var messageReceived = _messages[i];
+                    var timestampString = Encoding.Default.GetString(messageReceived.StartTimestampInChars, 0, messageReceived.MessageLength);
+                    var startTime = long.Parse(timestampString);
+                    fileStream.WriteLine(ToMicroseconds(messageReceived.EndTimestamp - startTime));
+                }
+            }
+        }
+
+        private static double ToMicroseconds(long ticks)
+        {
+            return unchecked(ticks * 1000000 / (double)(Stopwatch.Frequency));
+        }
+    }
+}
