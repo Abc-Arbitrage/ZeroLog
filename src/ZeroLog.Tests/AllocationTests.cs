7e0af53c5cc6fbd4842007f39d8d5c59d156b33c
MendelMonteiro <mendel.monteiro@gmail.com>
2017-01-28 15:15:17 +0100

    Add allocation test for most common scenario

---
+++ src/ZeroLog.Tests/AllocationTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Threading;
+using NFluent;
+using ZeroLog.Appenders;
+
+namespace ZeroLog.Tests
+{
+    [TestFixture]
+    public class AllocationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            foreach (var file in Directory.EnumerateFiles(@".\", $"allocation-test.*"))
+            {
+                File.Delete(file);
+            }
+
+            LogManager.Initialize(new[] {new DateAndSizeRollingFileAppender("allocation-test")}, 2048, 512);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            LogManager.Shutdown();
+        }
+
+        [Test]
+        public void should_not_allocate_using_all_formats_and_file_appender()
+        {
+            var log = LogManager.GetLogger("AllocationTest");
+
+            GC.Collect(2);
+
+            var gcCountBefore = GC.CollectionCount(0);
+
+            for (var i = 0; i < 2048 * 10; i++)
+            {
+                log.InfoFormat("Int {0}, Double {1:N4}, String {2}, Bool {3}, Decimal {4:N4}, Guid {5}, Timestamp {6}, DateTime {7}",
+                               123243,
+                               32423432.4398438,
+                               "Some random string",
+                               true,
+                               4234324324.23423423,
+                               Guid.NewGuid(),
+                               DateTime.UtcNow.TimeOfDay,
+                               DateTime.UtcNow);
+
+                Thread.Sleep(0);
+            }
+
+            // Give the appender some time to finish writing to file
+            Thread.Sleep(1000);
+
+            var gcCountAfter = GC.CollectionCount(0);
+
+            Check.That(gcCountBefore).IsEqualTo(gcCountAfter);
+        }
+    }
+}
