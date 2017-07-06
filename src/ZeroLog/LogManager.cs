776003f2436133ae7c4aefb8869b93cb1909bca7
Serge Farny <s.farny@abc-arbitrage.com>
2017-07-06 11:26:59 +0200

    Replace ConcurrentBag by ConcurrentQueue to avoid thread local storage

--- src/ZeroLog/LogManager.cs
+++ src/ZeroLog/LogManager.cs
@@ -16,7 +16,7 @@ namespace ZeroLog
         private static readonly IInternalLogManager _defaultLogManager = new NoopLogManager();
         private static IInternalLogManager _logManager = _defaultLogManager;
 
-        private readonly ConcurrentBag<Log> _loggers;
+        private readonly ConcurrentQueue<Log> _loggers;
         private readonly ConcurrentQueue<IInternalLogEvent> _queue;
         private readonly ObjectPool<IInternalLogEvent> _pool;
 
@@ -30,7 +30,7 @@ namespace ZeroLog
         internal LogManager(IConfigurationResolver configResolver, LogManagerConfiguration configuration)
         {
             _configResolver = configResolver;
-            _loggers = new ConcurrentBag<Log>();
+            _loggers = new ConcurrentQueue<Log>();
             _queue = new ConcurrentQueue<IInternalLogEvent>(new ConcurrentQueueCapacityInitializer(configuration.LogEventQueueSize));
 
             _bufferSegmentProvider = new BufferSegmentProvider(configuration.LogEventQueueSize * configuration.LogEventBufferSize, configuration.LogEventBufferSize);
@@ -128,7 +128,7 @@ namespace ZeroLog
         ILog IInternalLogManager.GetNewLog(IInternalLogManager logManager, string name)
         {
             var logger = new Log(logManager, name);
-            _loggers.Add(logger);
+            _loggers.Enqueue(logger);
             return logger;
         }
 
