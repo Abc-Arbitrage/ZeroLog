0f969c42e719508d66e310749916c2cb4c60b6db
Serge Farny <s.farny@abc-arbitrage.com>
2017-07-06 13:35:42 +0200

    Add benchmark (ZeroLog vs Log4Net vs NLog)

---
+++ src/ZeroLog.Benchmarks/Log4NetTestAppender.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using log4net.Appender;
+using log4net.Core;
+
+namespace ZeroLog.Benchmarks
+{
+    internal class Log4NetTestAppender : AppenderSkeleton
+    {
+        private readonly bool _captureLoggedMessages;
+        private int _messageCount;
+        private ManualResetEventSlim _signal;
+        private int _messageCountTarget;
+
+        public List<string> LoggedMessages { get; } = new List<string>();
+
+        public Log4NetTestAppender(bool captureLoggedMessages)
+        {
+            _captureLoggedMessages = captureLoggedMessages;
+        }
+
+        public ManualResetEventSlim SetMessageCountTarget(int expectedMessageCount)
+        {
+            _signal = new ManualResetEventSlim(false);
+            _messageCount = 0;
+            _messageCountTarget = expectedMessageCount;
+            return _signal;
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (_captureLoggedMessages)
+                LoggedMessages.Add(loggingEvent.ToString());
+
+            if (++_messageCount == _messageCountTarget)
+                _signal.Set();
+        }
+    }
+}
