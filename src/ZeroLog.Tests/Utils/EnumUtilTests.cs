226dc963707aa4f6f71342c6f1666e300cced914
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-03-28 19:24:45 +0200

    Add EnumUtil

---
+++ src/ZeroLog.Tests/Utils/EnumUtilTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NFluent;
+using NUnit.Framework;
+using ZeroLog.Utils;
+
+namespace ZeroLog.Tests.Utils
+{
+    public class EnumUtilTests
+    {
+        [Test]
+        public void should_round_trip_enum()
+        {
+            var typeHandle = EnumUtil.GetTypeHandle<DayOfWeek>();
+            var type = EnumUtil.GetTypeFromHandle(typeHandle);
+
+            Check.That(type).Equals(typeof(DayOfWeek));
+        }
+
+        [Test]
+        public void should_not_crash_on_invalid_handle()
+        {
+            Check.That(EnumUtil.GetTypeFromHandle(IntPtr.Zero)).IsNull();
+        }
+    }
+}
---
+++ src/ZeroLog/Utils/EnumUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using ExtraConstraints;
+using static System.Linq.Expressions.Expression;
+
+namespace ZeroLog.Utils
+{
+    internal static class EnumUtil
+    {
+        private static readonly Func<IntPtr, Type> _getTypeFromHandleFunc = BuildGetTypeFromHandleFunc();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IntPtr GetTypeHandle<[EnumConstraint] T>()
+            where T : struct
+            => Cache<T>.TypeHandle;
+
+        public static Type GetTypeFromHandle(IntPtr typeHandle)
+            => _getTypeFromHandleFunc?.Invoke(typeHandle);
+
+        private static Func<IntPtr, Type> BuildGetTypeFromHandleFunc()
+        {
+            var method = typeof(Type).GetMethod("GetTypeFromHandleUnsafe", BindingFlags.Static | BindingFlags.NonPublic, null, new[] { typeof(IntPtr) }, null);
+            if (method == null)
+                return null;
+
+            var param = Parameter(typeof(IntPtr));
+
+            return Lambda<Func<IntPtr, Type>>(
+                Call(method, param),
+                param
+            ).Compile();
+        }
+
+        private struct Cache<T>
+            where T : struct
+        {
+            public static readonly IntPtr TypeHandle = typeof(T).TypeHandle.Value;
+        }
+    }
+}
