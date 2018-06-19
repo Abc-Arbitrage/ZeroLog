c8d2eba23c97bd116cb2e3d15e972c65a13097e5
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-06-19 16:32:18 +0200

    Optimize prefix writing

--- src/ZeroLog/Appenders/AppenderBase.cs
+++ src/ZeroLog/Appenders/AppenderBase.cs
@@ -1,65 +1,26 @@
 using System;
 using System.IO;
 using System.Text;
-using System.Text.Formatting;
 using ZeroLog.Utils;
 
 namespace ZeroLog.Appenders
 {
     public abstract class AppenderBase<T> : IAppender<T>
     {
-        private readonly string[] _formatSpecifiers = { "%date", "%time", "%thread", "%level", "%logger" };
-        private readonly StringBuffer _stringBuffer;
-        private readonly byte[] _tempBytes;
         private Encoding _encoding;
         private byte[] _newlineBytes = ArrayUtil.Empty<byte>();
-        private string _prefixFormat;
-
-        protected AppenderBase()
-        {
-            _stringBuffer = new StringBuffer(256);
-            _tempBytes = new byte[512];
-        }
+        private PrefixWriter _prefixWriter;
 
         protected void Configure(string prefixPattern)
         {
-            _prefixFormat = BuildPrefixFormat(prefixPattern);
-        }
-
-        private string BuildPrefixFormat(string pattern)
-        {
-            var prefixFormat = pattern.Replace("%date", "{0:yyyy-MM-dd}");
-            for (var i = 1; i < _formatSpecifiers.Length; i++)
-            {
-                prefixFormat = prefixFormat.Replace(_formatSpecifiers[i], $"{{{i}}}");
-            }
-
-            return prefixFormat;
+            _prefixWriter = new PrefixWriter(prefixPattern);
         }
 
         protected int WriteEventToStream(Stream stream, ILogEventHeader logEventHeader, byte[] messageBytes, int messageLength)
         {
             var bytesWritten = 0;
-            bytesWritten += WritePrefix(stream, logEventHeader);
+            bytesWritten += _prefixWriter.WritePrefix(stream, logEventHeader, _encoding);
             bytesWritten += WriteLine(stream, messageBytes, messageLength);
-            return bytesWritten;
-        }
-
-        protected unsafe int WritePrefix(Stream stream, ILogEventHeader logEventHeader)
-        {
-            _stringBuffer.Clear();
-            _stringBuffer.AppendFormat(_prefixFormat,
-                                       logEventHeader.Timestamp.Date,
-                                       logEventHeader.Timestamp.TimeOfDay,
-                                       logEventHeader.ThreadId,
-                                       LevelStringCache.GetLevelString(logEventHeader.Level),
-                                       logEventHeader.Name);
-
-            int bytesWritten;
-            fixed (byte* buf = &_tempBytes[0])
-                bytesWritten = _stringBuffer.CopyTo(buf, _tempBytes.Length, 0, _stringBuffer.Count, _encoding);
-
-            stream.Write(_tempBytes, 0, bytesWritten);
             return bytesWritten;
         }
 
---
+++ src/ZeroLog/Appenders/PrefixWriter.cs
@@ -0,0 +1,274 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.Formatting;
+using System.Text.RegularExpressions;
+
+namespace ZeroLog.Appenders
+{
+    internal class PrefixWriter
+    {
+        private const string _dateFormat = "yyyy-MM-dd";
+
+        private readonly StringBuffer _stringBuffer = new StringBuffer(256);
+        private readonly byte[] _tempBytes = new byte[512];
+        private readonly char[] _strings;
+
+        private readonly Action<PrefixWriter, ILogEventHeader> _writePrefixMethod;
+
+        public PrefixWriter(string pattern)
+        {
+            var parts = ParsePattern(pattern).ToList();
+            BuildStrings(parts, out _strings, out var stringMap);
+            _writePrefixMethod = BuildAppendMethod(parts, stringMap);
+        }
+
+        private static IEnumerable<PatternPart> ParsePattern(string pattern)
+        {
+            foreach (var part in Regex.Split(pattern, @"(%(?:date|time|thread|level|logger)\b)", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase))
+            {
+                switch (part.ToLowerInvariant())
+                {
+                    case "":
+                        break;
+
+                    case "%date":
+                        yield return new PatternPart(PatternPartType.Date);
+                        break;
+
+                    case "%time":
+                        yield return new PatternPart(PatternPartType.Time);
+                        break;
+
+                    case "%thread":
+                        yield return new PatternPart(PatternPartType.Thread);
+                        break;
+
+                    case "%level":
+                        yield return new PatternPart(PatternPartType.Level);
+                        break;
+
+                    case "%logger":
+                        yield return new PatternPart(PatternPartType.Logger);
+                        break;
+
+                    default:
+                        yield return new PatternPart(part);
+                        break;
+                }
+            }
+        }
+
+        private static void BuildStrings(IEnumerable<PatternPart> parts, out char[] strings, out Dictionary<string, (int offset, int length)> map)
+        {
+            var stringOffsets = new Dictionary<string, (int offset, int length)>();
+            var stringsBuilder = new StringBuilder();
+
+            AddString(_dateFormat);
+
+            foreach (var part in parts)
+            {
+                if (part.Type != PatternPartType.String)
+                    continue;
+
+                AddString(part.Value);
+            }
+
+            void AddString(string value)
+            {
+                if (stringOffsets.ContainsKey(value))
+                    return;
+
+                var offset = stringsBuilder.Length;
+                stringsBuilder.Append(value);
+                stringOffsets[value] = (offset, value.Length);
+            }
+
+            strings = stringsBuilder.ToString().ToCharArray();
+            map = stringOffsets;
+        }
+
+        private static Action<PrefixWriter, ILogEventHeader> BuildAppendMethod(IEnumerable<PatternPart> parts, Dictionary<string, (int offset, int length)> stringMap)
+        {
+            var method = new DynamicMethod("WritePrefix", typeof(void), new[] { typeof(PrefixWriter), typeof(ILogEventHeader) }, typeof(PrefixWriter), true);
+            var il = method.GetILGenerator();
+
+            var stringBufferLocal = il.DeclareLocal(typeof(StringBuffer));
+            var stringsLocal = il.DeclareLocal(typeof(char).MakeByRefType(), true);
+            var stringsPtrLocal = il.DeclareLocal(typeof(char*));
+            var dateTimeLocal = il.DeclareLocal(typeof(DateTime));
+
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldfld, typeof(PrefixWriter).GetField(nameof(_stringBuffer), BindingFlags.Instance | BindingFlags.NonPublic));
+            il.Emit(OpCodes.Stloc, stringBufferLocal);
+
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldfld, typeof(PrefixWriter).GetField(nameof(_strings), BindingFlags.Instance | BindingFlags.NonPublic));
+            il.Emit(OpCodes.Ldc_I4_0);
+            il.Emit(OpCodes.Ldelema, typeof(char));
+            il.Emit(OpCodes.Stloc, stringsLocal);
+            il.Emit(OpCodes.Ldloc, stringsLocal);
+            il.Emit(OpCodes.Conv_U);
+            il.Emit(OpCodes.Stloc, stringsPtrLocal);
+
+            foreach (var part in parts)
+            {
+                switch (part.Type)
+                {
+                    case PatternPartType.String:
+                    {
+                        // _stringBuffer.Append(&_strings[0] + sizeof(char) * offset, length);
+
+                        var offsets = stringMap[part.Value];
+
+                        il.Emit(OpCodes.Ldloc, stringBufferLocal);
+
+                        il.Emit(OpCodes.Ldloc, stringsPtrLocal);
+                        il.Emit(OpCodes.Ldc_I4, sizeof(char) * offsets.offset);
+                        il.Emit(OpCodes.Add);
+
+                        il.Emit(OpCodes.Ldc_I4, offsets.length);
+
+                        il.Emit(OpCodes.Call, typeof(StringBuffer).GetMethod(nameof(StringBuffer.Append), new[] { typeof(char*), typeof(int) }));
+                        break;
+                    }
+
+                    case PatternPartType.Date:
+                    {
+                        // _stringBuffer.Append(logEventHeader.Timestamp, new StringView(&_strings[0] + sizeof(char) * offset, length));
+
+                        var formatOffsets = stringMap[_dateFormat];
+
+                        il.Emit(OpCodes.Ldloc, stringBufferLocal);
+
+                        il.Emit(OpCodes.Ldarg_1);
+                        il.Emit(OpCodes.Callvirt, typeof(ILogEventHeader).GetProperty(nameof(ILogEventHeader.Timestamp))?.GetGetMethod());
+
+                        il.Emit(OpCodes.Ldloc, stringsPtrLocal);
+                        il.Emit(OpCodes.Ldc_I4, sizeof(char) * formatOffsets.offset);
+                        il.Emit(OpCodes.Add);
+
+                        il.Emit(OpCodes.Ldc_I4, formatOffsets.length);
+
+                        il.Emit(OpCodes.Newobj, typeof(StringView).GetConstructor(new[] { typeof(char*), typeof(int) }));
+
+                        il.Emit(OpCodes.Call, typeof(StringBuffer).GetMethod(nameof(StringBuffer.Append), new[] { typeof(DateTime), typeof(StringView) }));
+                        break;
+                    }
+
+                    case PatternPartType.Time:
+                    {
+                        // _stringBuffer.Append(logEventHeader.Timestamp.TimeOfDay, StringView.Empty);
+
+                        il.Emit(OpCodes.Ldloc, stringBufferLocal);
+
+                        il.Emit(OpCodes.Ldarg_1);
+                        il.Emit(OpCodes.Callvirt, typeof(ILogEventHeader).GetProperty(nameof(ILogEventHeader.Timestamp))?.GetGetMethod());
+                        il.Emit(OpCodes.Stloc, dateTimeLocal);
+                        il.Emit(OpCodes.Ldloca, dateTimeLocal);
+                        il.Emit(OpCodes.Call, typeof(DateTime).GetProperty(nameof(DateTime.TimeOfDay))?.GetGetMethod());
+
+                        il.Emit(OpCodes.Ldsfld, typeof(StringView).GetField(nameof(StringView.Empty)));
+
+                        il.Emit(OpCodes.Call, typeof(StringBuffer).GetMethod(nameof(StringBuffer.Append), new[] { typeof(TimeSpan), typeof(StringView) }));
+                        break;
+                    }
+
+                    case PatternPartType.Thread:
+                    {
+                        // _stringBuffer.Append(logEventHeader.ThreadId, StringView.Empty);
+
+                        il.Emit(OpCodes.Ldloc, stringBufferLocal);
+
+                        il.Emit(OpCodes.Ldarg_1);
+                        il.Emit(OpCodes.Callvirt, typeof(ILogEventHeader).GetProperty(nameof(ILogEventHeader.ThreadId))?.GetGetMethod());
+
+                        il.Emit(OpCodes.Ldsfld, typeof(StringView).GetField(nameof(StringView.Empty)));
+
+                        il.Emit(OpCodes.Call, typeof(StringBuffer).GetMethod(nameof(StringBuffer.Append), new[] { typeof(int), typeof(StringView) }));
+                        break;
+                    }
+
+                    case PatternPartType.Level:
+                    {
+                        // _stringBuffer.Append(LevelStringCache.GetLevelString(logEventHeader.Level));
+
+                        il.Emit(OpCodes.Ldloc, stringBufferLocal);
+
+                        il.Emit(OpCodes.Ldarg_1);
+                        il.Emit(OpCodes.Callvirt, typeof(ILogEventHeader).GetProperty(nameof(ILogEventHeader.Level))?.GetGetMethod());
+                        il.Emit(OpCodes.Call, typeof(LevelStringCache).GetMethod(nameof(LevelStringCache.GetLevelString)));
+
+                        il.Emit(OpCodes.Call, typeof(StringBuffer).GetMethod(nameof(StringBuffer.Append), new[] { typeof(string) }));
+                        break;
+                    }
+
+                    case PatternPartType.Logger:
+                    {
+                        // _stringBuffer.Append(logEventHeader.Name);
+
+                        il.Emit(OpCodes.Ldloc, stringBufferLocal);
+
+                        il.Emit(OpCodes.Ldarg_1);
+                        il.Emit(OpCodes.Callvirt, typeof(ILogEventHeader).GetProperty(nameof(ILogEventHeader.Name))?.GetGetMethod());
+
+                        il.Emit(OpCodes.Call, typeof(StringBuffer).GetMethod(nameof(StringBuffer.Append), new[] { typeof(string) }));
+                        break;
+                    }
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            il.Emit(OpCodes.Ret);
+
+            return (Action<PrefixWriter, ILogEventHeader>)method.CreateDelegate(typeof(Action<PrefixWriter, ILogEventHeader>));
+        }
+
+        public unsafe int WritePrefix(Stream stream, ILogEventHeader logEventHeader, Encoding encoding)
+        {
+            _stringBuffer.Clear();
+            _writePrefixMethod(this, logEventHeader);
+
+            int bytesWritten;
+            fixed (byte* buf = &_tempBytes[0])
+                bytesWritten = _stringBuffer.CopyTo(buf, _tempBytes.Length, 0, _stringBuffer.Count, encoding);
+
+            stream.Write(_tempBytes, 0, bytesWritten);
+            return bytesWritten;
+        }
+
+        private enum PatternPartType
+        {
+            String,
+            Date,
+            Time,
+            Thread,
+            Level,
+            Logger
+        }
+
+        private struct PatternPart
+        {
+            public PatternPartType Type { get; }
+            public string Value { get; }
+
+            public PatternPart(PatternPartType type)
+            {
+                Type = type;
+                Value = null;
+            }
+
+            public PatternPart(string value)
+            {
+                Type = PatternPartType.String;
+                Value = value;
+            }
+        }
+    }
+}
