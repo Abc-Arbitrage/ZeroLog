f43b3db1c60c27de41ecdee78ef1318ec47287fa
Sergei Yemelianov <syemelyanov@deltixlab.com>
2019-09-19 16:27:07 +0300

    Add a test of resolving of an external appender (issue #34)

---
+++ src/ZeroLog.Tests.Externalappender/TestAppender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ZeroLog.Appenders;
+
+namespace ZeroLog.Tests.ExternalAppender
+{
+    public class TestAppender : ConsoleAppender
+    {
+        public TestAppender() {}
+
+        public TestAppender(string prefixPattern) : base(prefixPattern)
+        {
+        }
+
+        public override void Configure(DefaultAppenderConfig parameters)
+        {
+            parameters.PrefixPattern = String.Concat("(", Name, "): ", parameters.PrefixPattern);
+            base.Configure(parameters);
+        }
+    }
+}
---
+++ src/ZeroLog.Tests/Appenders/ExternalAppenderTest.cs
@@ -0,0 +1,46 @@
+﻿using Jil;
+using NUnit.Framework;
+using ZeroLog.Appenders;
+using ZeroLog.Config;
+using ZeroLog.ConfigResolvers;
+
+namespace ZeroLog.Tests.Appenders
+{
+    [TestFixture]
+    public class ExternalAppenderTest
+    {
+        [Test]
+        public void resolve_appender()
+        {
+            var appenderDef = new AppenderDefinition
+            {
+                Name = "ExtApp1",
+                AppenderTypeName = "ZeroLog.Tests.ExternalAppender.TestAppender,ZeroLog.Tests.ExternalAppender",
+                AppenderJsonConfig = new DefaultAppenderConfig { PrefixPattern = "[%level] @ %time - %logger: " }
+            };
+            var config = new ZeroLogJsonConfiguration
+            {
+                LogEventBufferSize = 5,
+                LogEventQueueSize = 7,
+                RootLogger = new LoggerDefinition
+                {
+                    Level = Level.Info,
+                    LogEventPoolExhaustionStrategy = LogEventPoolExhaustionStrategy.DropLogMessage,
+                    AppenderReferences = new[] { "ExtApp1" },
+
+                },
+                Appenders = new[] { appenderDef },
+            };
+            var configResolver = new HierarchicalResolver();
+            configResolver.Build(config);
+
+            Assert.DoesNotThrow(() => LogManager.Initialize(configResolver));
+
+            ILog logger = null;
+            Assert.DoesNotThrow(() => logger = LogManager.GetLogger(typeof(ExternalAppenderTest)));
+            Assert.DoesNotThrow(() => logger.Info("Logger initialized."));
+
+            LogManager.Shutdown();
+        }
+    }
+}
