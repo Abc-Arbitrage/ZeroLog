de2b2eed4d80caf4c2fc9887cd447691e3cfd717
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-01-05 20:20:41 +0100

    Remove ConcurrentQueue

--- src/ZeroLog/ObjectPool.cs
+++ src/ZeroLog/ObjectPool.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ZeroLog
 {
     internal class ObjectPool<T>
     {
-        private readonly ConcurrentQueue<T> _pool = new ConcurrentQueue<T>();
+        private readonly ConcurrentQueue<T> _pool = new();
         private int _poolSize;
 
         public ObjectPool(int size, Func<T> factory)
@@ -12,12 +14,10 @@ namespace ZeroLog
             _poolSize = size;
 
             for (var i = 0; i < size; i++)
-            {
                 _pool.Enqueue(factory());
-            }
         }
 
-        public bool TryAcquire(out T instance)
+        public bool TryAcquire([MaybeNullWhen(false)] out T instance)
             => _pool.TryDequeue(out instance);
 
         public void Release(T instance)
@@ -25,10 +25,8 @@ namespace ZeroLog
 
         public void Clear()
         {
-            while (_pool.TryDequeue(out _))
-            {
-                --_poolSize;
-            }
+            _pool.Clear();
+            _poolSize = 0;
         }
 
         public bool IsAnyItemAcquired()
