d73d10b73143d9771c7c0c6d50f2d2ddb866d0b6
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-01-20 20:45:56 +0100

    Report possible usages of string interpolation

---
+++ src/ZeroLog.Analyzers.Tests/UseStringInterpolationAnalyzerTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using NUnit.Framework;
+
+namespace ZeroLog.Analyzers.Tests;
+
+[TestFixture]
+public class UseStringInterpolationAnalyzerTests
+{
+    [Test]
+    public Task should_report_direct_log_opportunity()
+    {
+        var test = new Test
+        {
+            Source = @"
+class C
+{
+    void M(ZeroLog.Log log)
+        => log.{|#0:Info|}().Append(""Foo"").Log();
+}
+",
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(UseStringInterpolationAnalyzer.UseStringInterpolationDiagnostic).WithLocation(0)
+            }
+        };
+
+        return test.RunAsync();
+    }
+
+    [Test]
+    public Task should_report_interpolation_opportunity()
+    {
+        var test = new Test
+        {
+            Source = @"
+class C
+{
+    void M(ZeroLog.Log log)
+        => log.{|#0:Info|}().Append(42).AppendEnum(System.DayOfWeek.Friday).Append(true).Log();
+}
+",
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(UseStringInterpolationAnalyzer.UseStringInterpolationDiagnostic).WithLocation(0)
+            }
+        };
+
+        return test.RunAsync();
+    }
+
+    [Test]
+    public Task should_not_report_interpolation_opportunity_when_key_value_pairs_are_used()
+    {
+        var test = new Test
+        {
+            Source = @"
+class C
+{
+    void M(ZeroLog.Log log)
+        => log.Info().AppendKeyValue(""Key"", ""Value"").Log();
+}
+"
+        };
+
+        return test.RunAsync();
+    }
+
+    private class Test : ZeroLogAnalyzerTest<UseStringInterpolationAnalyzer>
+    {
+    }
+}
--- src/ZeroLog.Analyzers/DiagnosticIds.cs
+++ src/ZeroLog.Analyzers/DiagnosticIds.cs
@@ -6,4 +6,5 @@ internal static class DiagnosticIds
 
     public const string DiscardedLogMessage = "ZL0001";
     public const string AllocatingStringInterpolation = "ZL0002";
+    public const string UseStringInterpolation = "ZL0003";
 }
