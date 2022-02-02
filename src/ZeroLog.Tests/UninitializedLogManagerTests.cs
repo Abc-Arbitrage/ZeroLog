4de31f3710e79d8dbd01503dc9fba79757af2ea0
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2022-02-02 13:32:10 +0800

    Add unit test that checks that a logger retrieved before the log manager is initialised can log correctly

--- src/ZeroLog.Tests/UninitializedLogManagerTests.cs
+++ src/ZeroLog.Tests/UninitializedLogManagerTests.cs
@@ -1,10 +1,21 @@
+using System;
+using NFluent;
 using NUnit.Framework;
+using ZeroLog.Configuration;
 
 namespace ZeroLog.Tests
 {
-    [TestFixture]
+    [TestFixture, NonParallelizable]
     public class UninitializedLogManagerTests
     {
+        private TestAppender _testAppender;
+
+        [SetUp]
+        public void SetUpFixture()
+        {
+            _testAppender = new TestAppender(true);
+        }
+
         [TearDown]
         public void Teardown()
         {
@@ -15,6 +26,27 @@ namespace ZeroLog.Tests
         public void should_log_without_initialize()
         {
             LogManager.GetLogger("Test").Info($"Test");
+        }
+
+        [Test]
+        public void should_log_correctly_when_logger_is_retrieved_before_log_manager_is_initialized()
+        {
+            var log = LogManager.GetLogger<LogManagerTests>();
+
+            LogManager.Initialize(new ZeroLogConfiguration
+            {
+                LogMessagePoolSize = 10,
+                RootLogger =
+                {
+                    Appenders = { _testAppender }
+                }
+            });
+
+            var signal = _testAppender.SetMessageCountTarget(1);
+
+            log.Info("Lol");
+
+            Check.That(signal.Wait(TimeSpan.FromMilliseconds(100))).IsTrue();
         }
     }
 }
